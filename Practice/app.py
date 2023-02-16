65f0dbb97d1d92df18e8b5fc5487b86b770d1757
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-02-16 23:25:17 +0200

    Create app.py

---
+++ Practice/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:123456@localhost:5432/store'
+
+api = Api(app)
+
+db = SQLAlchemy(app)
+
+
+class BookModel(db.Model):
+    __tablename__ = 'books'
+    pk = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class BookResource(Resource):
+    def post(self):
+        data = request.get_json()
+        new_book = BookModel(**data)
+        db.session.add(new_book)
+        db.session.commit()
+        return new_book.as_dict()
+
+
+with app.app_context():
+    db.create_all()
+
+
+api.add_resource(BookResource, "/books/")
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
