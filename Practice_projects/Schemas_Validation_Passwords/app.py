0a031b6bc6ef007ed06ae45147fcc66effb6bf26
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2023-02-25 13:47:20 +0200

    Initial commit

--- Practice_projects/Schemas_Validation_Passwords/app.py
+++ Practice_projects/Schemas_Validation_Passwords/app.py
@@ -1,12 +1,101 @@
-from flask import Flask
+import enum
+
+from decouple import config
+from flask import Flask, request
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from marshmallow import Schema, fields
+
 
 app = Flask(__name__)
 
+db_user = config('DB_USER')
+db_password = config('DB_PASS')
+db_name = config('DB_NAME')
+db_port = config('DB_PORT')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}' \
+                                        f'@localhost:{db_port}/{db_name}'
+
+db = SQLAlchemy(app)
+api = Api(app)
+
+migrate = Migrate(app, db)
 
 
-if __name__ == '__main__':
-    app.run()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(
+        db.String(120),
+        nullable=False,
+        unique=True
+    )
+    password = db.Column(db.String(255), nullable=False)
+    full_name = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.Text)
+
+    # Use func.now()(=utc.now()) when saving datetime in db.
+    create_on = db.Column(db.DateTime, server_default=func.now())
+    updated_on = db.Column(db.DateTime, onupdate=func.now())
+
+
+class ColorEnum(enum.Enum):
+    pink = "pink"
+    black = "black"
+    white = "white"
+    yellow = "yellow"
+
+
+class SizeEnum(enum.Enum):
+    xs = "xs"
+    s = "s"
+    m = "m"
+    l = "l"
+    xl = "xl"
+    xxl = "xxl"
+
+
+class Clothes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    color = db.Column(
+        db.Enum(ColorEnum),
+        default=ColorEnum.white,
+        nullable=False
+    )
+    size = db.Column(
+        db.Enum(SizeEnum),
+        default=SizeEnum.s,
+        nullable=False
+    )
+    photo = db.Column(db.String(255), nullable=False)
+    create_on = db.Column(db.DateTime, server_default=func.now())
+    updated_on = db.Column(db.DateTime, onupdate=func.now())
+
+
+class BaseUserSchema(Schema):
+    email = fields.Email()
+    full_name = fields.String()
+
+
+class UserSignInSchema(BaseUserSchema):
+    password = fields.String()
+
+
+class SignUp(Resource):
+    def post(self):
+        data = request.get_json()
+        schema = UserSignInSchema()
+        errors = schema.validate(data)
+
+        if not errors:
+            db.session.add(User(**data))
+            db.session.commit()
+
+        return 201, data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
